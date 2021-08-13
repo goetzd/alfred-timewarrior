e357c6f606fa246204a768870944dc501c05dd37
Daniel Götz <daniel.goetz@tngtech.com>
2021-08-13 10:00:04 +0200

    :bug: Fix bug where rounding minutes up to the next full hour resulted in time stamps like "10:60" instead of "11:00"

--- src/timeUtils.ts
+++ src/timeUtils.ts
@@ -6,10 +6,18 @@ export const padTimeElement = (hoursOrMinutes: number) => {
 
 export const getDateStringWithRoundedGranularity = (date: Date) => {
   const roundedMinutes = Math.round(date.getMinutes() / configuration.granularityInMinutes) * configuration.granularityInMinutes;
-  return `${padTimeElement(date.getHours())}:${padTimeElement(roundedMinutes)}`;
+  return createHhMmString(date.getHours(), roundedMinutes);
 }
 
 export const getDateStringWithFlooredGranularity = (date: Date) => {
-  const roundedMinutes = Math.floor(date.getMinutes() / configuration.granularityInMinutes) * configuration.granularityInMinutes;
-  return `${padTimeElement(date.getHours())}:${padTimeElement(roundedMinutes)}`;
+  const flooredMinutes = Math.floor(date.getMinutes() / configuration.granularityInMinutes) * configuration.granularityInMinutes;
+  return createHhMmString(date.getHours(), flooredMinutes);
+}
+
+const createHhMmString = (hours: number, minutes: number): string => {
+  if (minutes >= 60) {
+    minutes = 0;
+    hours += 1;
+  }
+  return `${padTimeElement(hours)}:${padTimeElement(minutes)}`;
 }
